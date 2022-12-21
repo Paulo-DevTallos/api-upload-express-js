906708124832c7c91858ccf0bf8cba8c44234226
Paulo Sergio <pauloserg861@gmail.com>
2022-12-21 14:28:35 -0300

    informações complementares

--- testes/index.spec.js
+++ testes/index.spec.js
@@ -5,6 +5,12 @@ describe("Test router server", () => {
 	it("Should get main router", async () => {
 		const res = await request(app).get("/api/upload");
 
+		expect(res.statusCode).toEqual(200);
 		expect(res.body).toHaveProperty("message");
 	});
 });
+
+/*
+informações importantes para teste
+body, headers, statusCode, file, buffer
+*/
