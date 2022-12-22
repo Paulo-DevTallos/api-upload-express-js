5df1000ddd3fab8aa61b7be6cc67212a1de7d371
Paulo Sergio <pauloserg861@gmail.com>
2022-12-22 17:56:54 -0300

    habilitando logger do servidor

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require("./src/app");
 
 const port = process.env.PORT;
 
-app.listen(port, () =>
-  console.log(`server running on http://localhost:${port}`)
-);
+app.listen(port, () => {
+	console.log(`server running on http://localhost:${port}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const app = express();
+const logger = require("morgan");
 const db = require("./db");
 //router imports
 const uploadRouter = require("./routers/upload.router");
 
+const app = express();
+
 //db connection
 db.dbConnect();
+
 //set api
 app.use(
 	"/uploads",
@@ -15,6 +18,7 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "*");
